2484d5b156397f9e74d6dd56cd6cb06312836034
lizardiOmar <71732399+lizardiOmar@users.noreply.github.com>
2022-03-10 17:56:17 -0600

    Add files via upload

---
+++ axis2-calculadora/Calculadora.java
@@ -0,0 +1,17 @@
+public class Calculadora{
+    public static int suma(int a, int b){
+        return a+b;
+    }
+    public static int resta(int a, int b){
+        return a-b;
+    }
+    public static int multiplica(int a, int b){
+        return a*b;
+    }
+    public static double divide(int a, int b){
+        return a/b;
+    }
+	public static int cuadrado(int a){
+        return a*a;
+    }
+}
